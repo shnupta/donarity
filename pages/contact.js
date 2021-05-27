f8d75b206add4c9809b4b86577ee4a94c219ee07
Casey Williams <caseyjw@mail.com>
2021-05-27 17:25:39 +0100

    Add redirects with Auth0
    Also use session state inside header.
    
    Co-authored-by: moraru210 <moraru210@users.noreply.github.com>

---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+
+
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+export default function ContactPage()  {
+    return (
+        <>
+        <Head>
+            <title>Contact Us</title>
+        </Head>
+        <Layout>
+            <h1>Contact Us</h1>
+        </Layout>
+        </>
+    )
+}
