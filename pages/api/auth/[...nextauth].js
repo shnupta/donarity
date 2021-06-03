3d0e94d3bc784bcd184d5cbb8b5a7b95ab7fe454
Casey Williams <caseyjw@mail.com>
2021-06-03 10:14:15 +0100

    Remove unused import in nextauth.js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
-// import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import Adapters from "next-auth/adapters"
 
