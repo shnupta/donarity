4f51d415b6498fce098bd070375f194eaa6895dc
Casey Williams <caseyjw@mail.com>
2021-06-03 10:38:32 +0100

    Change class to className in carousel example

--- components/carousel.js
+++ components/carousel.js
@@ -3,11 +3,11 @@ import styles from "./carousel.module.css"
 export default function Carousel({children}) {
     return (
         <div className={styles.container}>
-            <div class={styles.item}>1</div>
-            <div class={styles.item}>2</div>
-            <div class={styles.item}>3</div>
-            <div class={styles.item}>4</div>
-            <div class={styles.item}>5</div>
+            <div className={styles.item}>1</div>
+            <div className={styles.item}>2</div>
+            <div className={styles.item}>3</div>
+            <div className={styles.item}>4</div>
+            <div className={styles.item}>5</div>
         </div>
     )
 }
