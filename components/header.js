ae657c0689868e21aee6abc9079344511af17ce4
Casey Williams <caseyjw@mail.com>
2021-06-04 10:06:00 +0100

    Use environment variable in auth callback url

--- components/header.js
+++ components/header.js
@@ -14,6 +14,7 @@ export default function Header() {
     const [session, loading] = useSession()
     const router = useRouter();
     const path = router.asPath;
+    const base_url = process.env.NEXT_PUBLIC_BASE_URL
 
     if (loading) return null
 
@@ -31,7 +32,7 @@ export default function Header() {
                 <Nav className={styles.links}>
                     {!session && (
                         <>
-                            <Nav.Link className={styles.navLink} onClick={() => signIn("auth0", { callbackUrl: 'http://localhost:3000/feed' })}>Login / Signup</Nav.Link>
+                            <Nav.Link className={styles.navLink} onClick={() => signIn("auth0", { callbackUrl: base_url + '/explore' })}>Login / Signup</Nav.Link>
                             <Nav.Link className={styles.navLink + (path === "/about" ? " " + styles.active : "")} href="/about">About</Nav.Link>
                             <Nav.Link className={styles.navLink + (path === "/charities" ? " " + styles.active : "")} href="/charities">Charities</Nav.Link>
                             <Nav.Link className={styles.navLink + (path === "/contact" ? " " + styles.active : "")} href="/contact">Contact Us</Nav.Link>
@@ -41,7 +42,7 @@ export default function Header() {
                         <>
                             <Nav.Link className={styles.navLink + (path === "/explore" ? " " + styles.active : "")} href="/explore">Explore</Nav.Link>
                             <Nav.Link className={styles.navLink + (path === "/manage" ? " " + styles.active : "")} href="/manage">Manage Donations</Nav.Link>
-                            <Nav.Link className={styles.navLink} onClick={() => signOut({ callbackUrl: 'http://localhost:3000/' })}>Sign Out</Nav.Link>
+                            <Nav.Link className={styles.navLink} onClick={() => signOut({ callbackUrl: base_url })}>Sign Out</Nav.Link>
                         </>
                     )}
                 </Nav>
