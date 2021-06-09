2778b02b9c5aff1b05896bb63e8188dca8743882
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2021-06-09 17:41:16 +0100

    Modify ArrowLink component to choose left or right or no arrow

--- components/arrow-link.js
+++ components/arrow-link.js
@@ -1,9 +1,13 @@
 import styles from './arrow-link.module.css'
 import Link from 'next/link'
 
-export default function ArrowLink({ children, href }) {
+export default function ArrowLink({ children, href, right, left, className }) {
 
     return (
-        <Link href={href}><a className={styles.link}>{children}</a></Link>
+        <Link href={href} passHref={true}>
+            <a className={styles.link + (right ? " " + styles.right : "") + (left ? " " + styles.left : "") + (className ? " " + className : "")}>
+                {children}
+            </a>
+        </Link>
     )
 }
