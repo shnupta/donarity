6299b193fbf86e286e71ad860033144b83e6466a
Casey Williams <caseyjw@mail.com>
2021-06-09 16:13:59 +0100

    Add a basic stripe checkout to donate

--- pages/donate/[id].js
+++ pages/donate/[id].js
@@ -7,6 +7,8 @@ import PaymentForm from "../../components/payment-form";
 import prisma from "../../lib/prisma";
 import Router from "next/router";
 
+import getStripe from "../../lib/stripe"
+
 export const getServerSideProps = async (context) => {
   // Find the model in prisma/schema.prisma
   const charity = await prisma.charity.findUnique({
@@ -29,16 +31,40 @@ export default function DonatePage({ charity }) {
     if (session) {
       body.userId = session.userId;
     }
-    try {
-      await fetch(process.env.NEXT_PUBLIC_BASE_URL + `/api/donate`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      });
-      await Router.push("/thankyou");
-    } catch (error) {
-      console.error(error);
+
+    const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/api/checkout_sessions', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    })
+
+    if (response.status === 500) {
+      console.error(response.statusText)
+      return
     }
+
+    const stripe = await getStripe()
+    if (!stripe) {
+      console.error("Couldn't load stripe.")
+      return
+    }
+    const resBody = await response.json() 
+    const { error } = await stripe.redirectToCheckout({
+      sessionId: await resBody.id
+    })
+
+    console.warn(error.message)
+
+    // try {
+    //   await fetch(process.env.NEXT_PUBLIC_BASE_URL + `/api/donate`, {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(body),
+    //   });
+    //   await Router.push("/thankyou");
+    // } catch (error) {
+    //   console.error(error);
+    // }
   };
 
   return (
