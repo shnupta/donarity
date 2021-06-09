e8bbe0e245a309d3859dc7b4474e0af8a354d567
Casey Williams <caseyjw@mail.com>
2021-06-09 18:51:18 +0100

    Conditionally render recurring buttons if session

--- pages/donate/[id].js
+++ pages/donate/[id].js
@@ -5,9 +5,9 @@ import Button from "../../components/button";
 import CharitySummary from "../../components/charity-summary";
 import PaymentForm from "../../components/payment-form";
 import prisma from "../../lib/prisma";
-import Router from "next/router";
 
 import getStripe from "../../lib/stripe"
+import { userSession } from "lib/session";
 
 export const getServerSideProps = async (context) => {
   // Find the model in prisma/schema.prisma
@@ -17,14 +17,21 @@ export const getServerSideProps = async (context) => {
     },
   });
 
-  // TODO: Check if found if not throw 404 page
+  if (!charity) {
+    return {
+      redirect: {
+        destination: '/404',
+        permanent: false
+      }
+    }
+  }
 
-  return { props: { charity } };
+  const session = await userSession(context.req);
+
+  return { props: { charity, session } };
 };
 
-export default function DonatePage({ charity }) {
-  const [session, loading] = useSession();
-
+export default function DonatePage({ charity, session }) {
   const submitData = async (data) => {
     const body = data;
     body.charityId = charity.id;
@@ -54,17 +61,6 @@ export default function DonatePage({ charity }) {
     })
 
     console.warn(error.message)
-
-    // try {
-    //   await fetch(process.env.NEXT_PUBLIC_BASE_URL + `/api/donate`, {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify(body),
-    //   });
-    //   await Router.push("/thankyou");
-    // } catch (error) {
-    //   console.error(error);
-    // }
   };
 
   return (
@@ -72,7 +68,7 @@ export default function DonatePage({ charity }) {
       <CharitySummary className={styles.summary} charity={charity} />
       <h1>New Donation</h1>
 
-      <PaymentForm handler={submitData} />
+      <PaymentForm handler={submitData} session={session} />
 
       <h1 className={styles.or}>Or</h1>
       <Button className={styles.donate}>Add To Split Donations</Button>
