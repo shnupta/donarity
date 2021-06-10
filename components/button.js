ab818fc1b7a036199a2493fbd1a0cff3366b61c5
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2021-06-10 09:51:31 +0100

    Modify Button component to allow for icon

--- components/button.js
+++ components/button.js
@@ -1,11 +1,12 @@
 import styles from './button.module.css'
 
-export default function Button({ children, className, onClick, white }) {
+export default function Button({ children, className, onClick, white, icon }) {
     return (
         <button
           className={styles.button + (className ? " " + className : "") + (white ? " " + styles.white : "")}
           onClick={onClick ? onClick : () => {}}
         >
+          {icon && <img src={icon} />}
           {children}
         </button>
     )
