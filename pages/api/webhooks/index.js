aec9459501ee2c2540fc329d3d7ebee1e6166626
Casey Williams <caseyjw@mail.com>
2021-06-10 20:39:58 +0100

    Wrap handleCheckoutSessionCompleted in try catch

--- pages/api/webhooks/index.js
+++ pages/api/webhooks/index.js
@@ -1,77 +1,84 @@
-import Cors from 'micro-cors'
-import { buffer } from 'micro'
-import prisma from 'lib/prisma'
-import { getServerStripe } from 'lib/serverStripe'
+import Cors from "micro-cors";
+import { buffer } from "micro";
+import prisma from "lib/prisma";
+import { getServerStripe } from "lib/serverStripe";
 
-const stripe = getServerStripe(); 
+const stripe = getServerStripe();
 
 const cors = Cors({
-	methods: ['POST', 'HEAD']
-})
+  methods: ["POST", "HEAD"],
+});
 
 export const config = {
   api: {
-    bodyParser: false, 
-  }
-}
+    bodyParser: false,
+  },
+};
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 async function handleCheckoutSessionCompleted(session) {
-  const checkoutSession = await prisma.checkoutSession.findUnique({
-    where: {
-      sessionId: session.id
-    }
-  })
+  try {
+    const checkoutSession = await prisma.checkoutSession.findUnique({
+      where: {
+        sessionId: session.id,
+      },
+    });
 
-  console.log("Got checkout session:")
-  console.log(checkoutSession)
+    console.log("Got checkout session:");
+    console.log(checkoutSession);
 
-  const donation = await prisma.donation.create({
-    data: {
-      frequency: checkoutSession.donationFrequency,
-      amount: checkoutSession.amount,
-      charityId: checkoutSession.charityId,
-      userId: checkoutSession.userId
-    }
-  })
+    const donation = await prisma.donation.create({
+      data: {
+        frequency: checkoutSession.donationFrequency,
+        amount: checkoutSession.amount,
+        charityId: checkoutSession.charityId,
+        userId: checkoutSession.userId,
+      },
+    });
 
-  console.log("Created donation:")
-  console.log(donation)
+    console.log("Created donation:");
+    console.log(donation);
 
-
-  const updatedSession = await prisma.checkoutSession.update({
-    where: {
-      sessionId: session.id
-    },
-    data: {
-      stripeCustomerId: session.customer,
-      donationId: donation.id,
-      completed: true
-    }
-  })
-}
-
-async function handler(req, res) {
-  if (req.method === 'POST') {
-    const buf = await buffer(req) 
-    const sig = req.headers["stripe-signature"]
-
-    let event
-
-    try {
-      event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret)
-    } catch (err) {
-      console.error(err.message)
-      res.status(400).send(`Webhook error: ${err.message}`)
-      return
-    }
-
-    if (event.type === 'checkout.session.completed') {
-      handleCheckoutSessionCompleted(event.data.object)
-    }
-    res.status(200).json({received: true})
+    const updatedSession = await prisma.checkoutSession.update({
+      where: {
+        sessionId: session.id,
+      },
+      data: {
+        stripeCustomerId: session.customer,
+        donationId: donation.id,
+        completed: true,
+      },
+    });
+  } catch (err) {
+    console.error(err);
   }
 }
 
-export default cors(handler)
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const sig = req.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf.toString(),
+        sig,
+        webhookSecret
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(400).send(`Webhook error: ${err.message}`);
+      return;
+    }
+
+    if (event.type === "checkout.session.completed") {
+      handleCheckoutSessionCompleted(event.data.object);
+    }
+    res.status(200).json({ received: true });
+  }
+}
+
+export default cors(handler);
