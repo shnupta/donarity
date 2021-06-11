61a3e27363ecd2f2966e40e398729aa0a8dc597c
Casey Williams <caseyjw@mail.com>
2021-06-11 11:53:33 +0100

    Get charity and donation info for thank you page

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -1,31 +1,46 @@
-
-import Head from 'next/head'
-import Layout from '../components/layout'
-import PageTitle from '../components/page-title'
-import Button from '../components/button'
-import Router from 'next/router'
-import { getServerStripe } from 'lib/serverStripe'
+import Head from "next/head";
+import Layout from "../components/layout";
+import PageTitle from "../components/page-title";
+import Button from "../components/button";
+import Router from "next/router";
+import prisma from 'lib/prisma'
 
 export async function getServerSideProps({ query }) {
-    const stripe = getServerStripe();
-    const session = await stripe.checkout.sessions.retrieve(query.session_id)
+  var checkoutSession = await prisma.checkoutSession.findUnique({
+    where: {
+      sessionId: query.session_id,
+    },
+    include: {
+      charity: true,
+    },
+  });
 
-    return {
-        props: {}
-    }
+  console.log(checkoutSession);
+
+  checkoutSession = JSON.parse(JSON.stringify(checkoutSession, 
+    (key, value) => (typeof value === 'Decimal' ? value.toString() : value)))
+
+  return {
+    props: { checkoutSession },
+  };
 }
 
-export default function ThankYouPage() {
-    return (
-        <>
-        <Head>
-            <title>Thank You!</title>
-        </Head>
-        <Layout>
-            <PageTitle>Thank You</PageTitle>
-            <h4>Your donation will make a big difference!</h4>
-            <Button onClick={() => Router.push('/explore')}>Head Home</Button>
-        </Layout>
-        </>
-    )
+export default function ThankYouPage({ checkoutSession }) {
+  return (
+    <>
+      <Head>
+        <title>Thank You!</title>
+      </Head>
+      <Layout>
+        <PageTitle>
+          Thank you for your donation to {checkoutSession.charity.name}
+        </PageTitle>
+        <h4>
+          Your £{checkoutSession.amount} donation will make a big difference!
+        </h4>
+
+        <Button onClick={() => Router.push("/explore")}>Head Home</Button>
+      </Layout>
+    </>
+  );
 }
