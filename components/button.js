0164d92334bba3919f8c25b46e44d014b1e5bc28
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2021-06-16 10:45:13 +0100

    Add clear all filters button

--- components/button.js
+++ components/button.js
@@ -1,9 +1,10 @@
 import styles from './button.module.css'
 
-export default function Button({ children, className, onClick, white, icon }) {
+export default function Button({ children, className, onClick, white, icon, disabled }) {
     return (
         <button
-          className={styles.button + (className ? " " + className : "") + (white ? " " + styles.white : "")}
+          disabled={disabled}
+          className={styles.button + (className ? " " + className : "") + (white ? " " + styles.white : "") + (disabled ? " " + styles.disabled : "")}
           onClick={onClick ? onClick : () => {}}
         >
           {icon && <img src={icon} className={children ? "" : styles.iconOnly} />}
--- pages/explore.js
+++ pages/explore.js
@@ -11,11 +11,11 @@ import { UserRole } from "@prisma/client";
 import React from "react";
 import { getSession } from "next-auth/client";
 import prisma from "lib/prisma";
-import { InstantSearch, SortBy, RefinementList } from 'react-instantsearch-dom';
+import { InstantSearch, SortBy, ClearRefinements, MenuSelect } from 'react-instantsearch-dom';
 import algolia from 'lib/algolia';
 import Modal from '../components/modal';
 import Button from '../components/button';
-import RangeSlider from '../components/range-slider';
+import ClearFilters from '../components/clear-filters';
 import SizeFilter from '../components/size-filter';
 import ScopeSelector from "../components/scope-selector";
 
@@ -94,8 +94,10 @@ export default function ExplorePage({ charities }) {
                 <Button icon="/filter.svg" className={styles.filterButton} onClick={() => setFiltersOpen(true)}>Filter</Button>
                 <Modal open={filtersOpen} onClose={() => setFiltersOpen(false)}>
                   <h1>Filters</h1>
+                  <MenuSelect attribute="category.name" />
                   <SizeFilter className={styles.filterTile} attribute="size" min={0} max={999999999} />
                   <ScopeSelector className={styles.filterTile} attribute="scope" />
+                  <ClearFilters className={styles.clear} />
                 </Modal>
               </div>
               <ExploreGrid charities={charities} />
